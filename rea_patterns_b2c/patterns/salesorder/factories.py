cdcd2755c1933a87403fab313ff596d2a2956343
Y3K <dboy.y3k@gmail.com>
2014-11-04 23:21:47 -0600

    Remove faker

--- rea_patterns_b2c/patterns/salesorder/factories.py
+++ rea_patterns_b2c/patterns/salesorder/factories.py
@@ -2,14 +2,10 @@ import factory
 
 from django.contrib.webdesign import lorem_ipsum
 
-from faker import Factory
-
-
-fake = Factory.create()
-
 
 class SalesOrderFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = 'salesorder.SalesOrder'
 
     title = factory.LazyAttribute(
-        lambda o: lorem_ipsum.words(5, common=False).title())
+        lambda o: lorem_ipsum.words(5, common=False).title()
+    )
